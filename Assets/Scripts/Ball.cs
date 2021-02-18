a125b4442623fb1f453535d03c9049d731faa430
ben dadon <bendadon123@gmail.com>
2021-02-18 08:21:41 +0200

    Walls, Blocks and bounciness

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Paddle paddle;
+    [SerializeField] float pushX = 2f;
+    [SerializeField] float pushY = 15f;
+    Vector2 paddleToBallVector;
+    bool hasStarted = false;
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!hasStarted) 
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+        
+    }
+
+    private void LockBallToPaddle() {
+        Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void LaunchOnMouseClick() {
+        if(Input.GetMouseButtonDown(0)) 
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(pushX, pushY);
+        }
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minPosX = 1f;
+    [SerializeField] float maxPosX = 15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float posInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(posInUnits, minPosX, maxPosX);
+        transform.position = paddlePos;
+    }
+}
